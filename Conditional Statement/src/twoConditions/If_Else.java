2760c14497e0068336dbb9a054f59bf00f52fdd4
SyedShumon <57926882+SyedShumon@users.noreply.github.com>
2022-07-12 15:49:00 -0400

    Adding files

---
+++ Conditional Statement/src/twoConditions/If_Else.java
@@ -0,0 +1,38 @@
+package twoConditions;
+
+public class If_Else {
+
+	public static void main(String[] args) {
+		
+		//One Condition
+		
+		int x = 20;
+		
+		if(x>=20) {
+			
+			System.out.println("Test pass");
+			
+			
+		}
+		
+		//Two Condition's (else)
+		
+		
+				int y = 20;
+				
+				if(y>=40) {
+					
+					System.out.println("Test pass");
+					
+			}else {
+				
+				System.out.println("Test fail");
+				
+				
+			}
+		
+		
+
+	}
+
+}
---
+++ PratciseTestNG/src/flickerSignuptest/ParametarizationTest.java
@@ -0,0 +1,28 @@
+package flickerSignuptest;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParametarizationTest {
+	
+	
+	
+	
+	@Parameters({"firstName","lastName"})
+	@Test
+	public void attest(String firstName, String lastName) {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
